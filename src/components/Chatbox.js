e8d63004ae9033af944148757fd91a43acf58eee
Elliott <elliottvincentparedes@gmail.com>
2022-04-07 22:17:53 -0500

    readded nowrap

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -103,7 +103,7 @@ const Chatbox = () =>
     
     <div className="conatiner" style={{backgroundColor: "white", height:"100vh"}}>
           <div className="card center p-0 m-0 border-none" style={{backgroundColor:"white", width:"800px"}}>
-            <div className="row">
+            <div className="row flex-nowrap">
               <div className="col-lg-4" style={{backgroundColor:"white"}}>
                 <div className="row border-1" style={{height:"3rem"}}>
                   <div className="col nav">
